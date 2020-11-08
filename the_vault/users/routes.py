34d41ad9be4f9fe4d64736ac4c0db6c52c3b99b3
Starlight Romero <starlightromero@gmail.com>
2020-11-08 14:47:18 -0800

    removed unused forms

--- the_vault/users/routes.py
+++ the_vault/users/routes.py
@@ -3,10 +3,5 @@ from flask import render_template, Blueprint, redirect, url_for, flash
 from flask_login import login_required, current_user, login_user, logout_user
 from the_vault import db
 from the_vault.models import User
-from the_vault.users.forms import (
-    RegistrationForm,
-    LoginForm,
-    UpdateAccountForm,
-)
 
 users = Blueprint("users", __name__)
